adaa2bbf201706d408ba872594ba115214c49a89
Yifei Sun <ysun@hey.com>
2026-03-23 22:20:19 +0100

    pkgs/bird3: fix patch

--- pkgs/bird3/default.nix
+++ pkgs/bird3/default.nix
@@ -4,10 +4,16 @@
 }:
 
 pkgsPrev.bird3.overrideAttrs (oldAttrs: {
-  patches = oldAttrs ++ [
+  patches = (oldAttrs.patches or [ ]) ++ [
+    # rtt: https://github.com/nickcao/bird
     (fetchpatch2 {
       url = "https://github.com/nickcao/bird/commit/2912d03c99f99bbe2f7d5041b43a551d3156ce93.patch";
-      hash = "";
+      hash = "sha256-osVfPQAw5qcLhirGtSZDSUA8/ZosJNz1utwhO87sL8c=";
+    })
+    # https://github.com/nickcao/flakes/blob/master/pkgs/bird-babel-rtt/fix-vrf-if-delete-notification.patch
+    (fetchpatch2 {
+      url = "https://raw.githubusercontent.com/nickcao/flakes/3358dc18efa4478f5eef69803d6e38fbbc4fe248/pkgs/bird-babel-rtt/fix-vrf-if-delete-notification.patch";
+      hash = "sha256-Deyz+6kAxlQr5sDBEnKY4KbeafDHzw+RJHm9G9MkypM=";
     })
   ];
 })
