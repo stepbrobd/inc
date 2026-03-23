56f763a5a096742a7f4d5ded7d7dd90003bdb8b6
Yifei Sun <ysun@hey.com>
2026-03-23 23:14:55 +0100

    lib/blueprint: more metadata

--- lib/blueprint.nix
+++ lib/blueprint.nix
@@ -20,15 +20,17 @@ let
     , type # e.g. "laptop", "desktop", "server", "rpi"
     , domain ? "sd.ysun.co"
     , tags ? [ ]
-    , interface ? null # e.g. "eth0", "enp1s0" — primary outbound network interface
+    , interface ? null # e.g. "eth0", "enp1s0" primary outbound network interface
     , ipv4 ? null
     , ipv6 ? null
+    , name ? null # e.g. "Butte" display name
+    , meta ? { } # e.g. { country, region, city, postal, continent }
     , ipam ? { }
     , ranet ? { }
     , services ? { }
     }: {
       inherit platform os provider type; # metadata
-      inherit hostName domain interface ipv4 ipv6 ipam ranet; # networking
+      inherit hostName name domain interface ipv4 ipv6 ipam ranet meta; # networking
       inherit services;
       fqdn = "${hostName}.${domain}";
       tags = [ "server" ] ++ tags;
@@ -69,12 +71,14 @@ in
 
   # servers
   hosts.butte = newHost (lib.fix (self: {
+    name = "Butte";
     hostName = "butte";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "virtua";
     type = "server";
     tags = [ "anycast" "router" "ranet" ];
+    meta = { city = "Paris"; region = "FR-IDF"; country = "FR"; continent = "Europe"; postal = "75000"; };
     interface = "eth0";
     ipv4 = "185.234.100.120";
     ipv6 = "2a07:8dc0:1c:0:48:f1ff:febe:1c6";
@@ -89,12 +93,14 @@ in
   }));
 
   hosts.halti = newHost (lib.fix (self: {
+    name = "Halti";
     hostName = "halti";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "garnix";
     type = "server";
     tags = [ "routee" "grafana" "ranet" ];
+    meta = { city = "Helsinki"; region = "FI-18"; country = "FI"; continent = "Europe"; postal = "00100"; };
     interface = "enp1s0";
     ipv4 = "37.27.181.83";
     ipv6 = "2a01:4f9:c012:7b3a::1";
@@ -109,12 +115,14 @@ in
   }));
 
   hosts.isere = newHost (lib.fix (self: {
+    name = "Isere";
     hostName = "isere";
     platform = "aarch64-linux";
     os = "nixos";
     provider = "owned";
     type = "rpi";
     tags = [ "routee" "home-assistant" "vaultwarden" "ntpd-rs" "ranet" ];
+    meta = { city = "Grenoble"; region = "FR-ARA"; country = "FR"; continent = "Europe"; postal = "38000"; };
     interface = "end0";
     ipv4 = "88.140.186.193";
     ipv6 = "2001:470:1f12:441::2";
@@ -129,12 +137,14 @@ in
   }));
 
   hosts.highline = newHost (lib.fix (self: {
+    name = "Highline";
     hostName = "highline";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "neptune";
     type = "server";
     tags = [ "anycast" "router" "ranet" ];
+    meta = { city = "New York City"; region = "US-NY"; country = "US"; continent = "North America"; postal = "10001"; };
     interface = "ens3";
     ipv4 = "172.82.22.183";
     ipv6 = "2602:fe2e:4:b2:fd:87ff:fe11:53cb";
@@ -149,12 +159,14 @@ in
   }));
 
   hosts.kongo = newHost (lib.fix (self: {
+    name = "Kongo";
     hostName = "kongo";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "vultr";
     type = "server";
     tags = [ "anycast" "router" "ranet" ];
+    meta = { city = "Osaka"; region = "JP-27"; country = "JP"; continent = "Asia"; postal = "540-0001"; };
     interface = "enp1s0";
     ipv4 = "45.32.59.137";
     ipv6 = "2001:19f0:7002:0327:5400:05ff:febb:599b";
@@ -169,12 +181,14 @@ in
   }));
 
   hosts.lagern = newHost (lib.fix (self: {
+    name = "Lagern";
     hostName = "lagern";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "aws";
     type = "server";
     tags = [ "routee" "jitsi" "ranet" ];
+    meta = { city = "Zurich"; region = "CH-ZH"; country = "CH"; continent = "Europe"; postal = "8001"; };
     interface = "ens5";
     ipv4 = "16.62.113.214";
     ipv6 = "2a05:d019:b00:b6f0:6981:b7c5:ff97:9eea";
@@ -189,12 +203,14 @@ in
   }));
 
   hosts.odake = newHost (lib.fix (self: {
+    name = "Odake";
     hostName = "odake";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "ssdnodes";
     type = "server";
     tags = [ "routee" "attic" "hydra" "neogrok" "ranet" ];
+    meta = { city = "Tokyo"; region = "JP-13"; country = "JP"; continent = "Asia"; postal = "100-0001"; };
     interface = "enp3s0";
     ipv4 = "209.182.234.194";
     ipv6 = "2602:ff16:14:0:1:56:0:1";
@@ -209,12 +225,14 @@ in
   }));
 
   hosts.timah = newHost (lib.fix (self: {
+    name = "Timah";
     hostName = "timah";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "misaka";
     type = "server";
     tags = [ "anycast" "router" "ranet" ];
+    meta = { city = "Singapore"; region = "SG"; country = "SG"; continent = "Asia"; postal = "139963"; };
     interface = "enp3s0";
     ipv4 = "194.114.138.187";
     ipv6 = "2407:b9c0:e002:25c:26a3:f0ff:fe45:a7b7";
@@ -229,12 +247,14 @@ in
   }));
 
   hosts.toompea = newHost (lib.fix (self: {
+    name = "Toompea";
     hostName = "toompea";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "xtom";
     type = "server";
     tags = [ "anycast" "router" "calibre" "plausible" "ranet" ];
+    meta = { city = "Tallinn"; region = "EE-37"; country = "EE"; continent = "Europe"; postal = "10111"; };
     interface = "enp6s18";
     ipv4 = "185.194.53.29";
     ipv6 = "2a04:6f00:4::a5";
@@ -249,12 +269,14 @@ in
   }));
 
   hosts.walberla = newHost (lib.fix (self: {
+    name = "Walberla";
     hostName = "walberla";
     platform = "x86_64-linux";
     os = "nixos";
     provider = "hetzner";
     type = "server";
     tags = [ "routee" "glance" "golink" "kanidm" "ranet" ];
+    meta = { city = "Falkenstein"; region = "DE-SN"; country = "DE"; continent = "Europe"; postal = "08223"; };
     interface = "eth0";
     ipv4 = "23.88.126.45";
     ipv6 = "2a01:4f8:c17:4b75::1";
