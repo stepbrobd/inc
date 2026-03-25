fb9a31d76e22bb4ba803b412e19ba93ef97d4ba8
Yifei Sun <ysun@hey.com>
2026-03-25 17:05:42 +0100

    hosts/{framework,xps}: power profile tuning

--- hosts/laptop/xps/hardware.nix
+++ hosts/laptop/xps/hardware.nix
@@ -53,10 +53,10 @@
       STOP_CHARGE_THRESH_BAT0 = 75;
       START_CHARGE_THRESH_BAT1 = 70;
       STOP_CHARGE_THRESH_BAT1 = 75;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
     };
   };
   services.thermald.enable = true;
