f456d82cd13c013ba3801b42631698cba4bece28
Yifei Sun <ysun@hey.com>
2026-03-25 13:37:47 +0100

    modules/home/hyprland: strip rofi dunst waybar from hyprland

--- modules/home/ysun/hyprland/default.nix
+++ modules/home/ysun/hyprland/default.nix
@@ -1,21 +1,39 @@
-{ inputs, lib, ... }:
+{ inputs, ... }:
 
 { pkgs
+, config
 , osConfig ? { services.desktopManager.enabled = null; }
 , ...
 }:
 
 {
-  imports = [
-    ./dunst.nix
-    ./rofi.nix
-    ./theme.nix
-    # ./waybar.nix
-    ./wpaperd.nix
+  imports = [ inputs.self.homeManagerModules.ysun.noctalia ];
 
-    inputs.noctalia.homeModules.default
-    ./noctalia.nix
-  ];
+  gtk = {
+    enable = true;
+    gtk4.theme = config.gtk.theme;
+
+    theme = {
+      package = pkgs.nordic;
+      name = "Nordic";
+    };
+
+    cursorTheme = {
+      package = pkgs.nordzy-cursor-theme;
+      name = "Nordzy-cursors";
+      size = 24;
+    };
+  };
+
+  home.pointerCursor = {
+    size = 24;
+
+    package = pkgs.nordzy-cursor-theme;
+    name = "Nordzy-cursors";
+
+    gtk.enable = true;
+    x11.enable = true;
+  };
 
   home.packages = with pkgs; [
     brightnessctl
