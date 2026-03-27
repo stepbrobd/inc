41e8039c2b86563beaf97298b30e0108dd66d32d
Yifei Sun <ysun@hey.com>
2026-03-27 17:03:05 +0100

    modules/flake/formatter: allow running in any dir and check for secrets

--- modules/flake/formatter.nix
+++ modules/flake/formatter.nix
@@ -1,8 +1,24 @@
 {
   perSystem = { lib, pkgs, ... }: {
     formatter = pkgs.writeShellScriptBin "formatter" ''
+      set -eoux pipefail
+      shopt -s globstar
+
+      root="$PWD"
+      while [[ ! -f "$root/.git/index" ]]; do
+        if [[ "$root" == "/" ]]; then
+          exit 1
+        fi
+        root="$(dirname "$root")"
+      done
+      pushd "$root" > /dev/null
+
       ${lib.getExe pkgs.deno} fmt .
+      ${lib.getExe pkgs.gitleaks} git . --verbose
       ${lib.getExe pkgs.nixpkgs-fmt} .
+      ${lib.getExe pkgs.taplo} format **/*.toml
+
+      popd
     '';
   };
 }
