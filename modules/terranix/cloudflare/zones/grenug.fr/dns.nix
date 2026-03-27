a6592cc5e46b7964b7f6025e02b7ad0767c2f330
Yifei Sun <ysun@hey.com>
2026-03-27 15:23:31 +0100

    modules/terranix/cloudflare: remove unused records

--- modules/terranix/cloudflare/zones/grenug.fr/dns.nix
+++ modules/terranix/cloudflare/zones/grenug.fr/dns.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 
 let
-  inherit (lib.terranix) forZone mkPurelyMailRecord tfRef;
+  inherit (lib.terranix) forZone mkPurelyMailRecord;
 in
 {
   resource.cloudflare_dns_record = forZone "grenug.fr"
@@ -12,22 +12,6 @@ in
         name = "*";
         content = "grenug.fr";
         comment = "Cloudflare Workers - Grenuble Nix User Group";
-      };
-
-      fr_grenug_meet = {
-        type = "CNAME";
-        name = "meet";
-        proxied = tfRef "cloudflare_dns_record.co_ysun_meet.proxied";
-        content = tfRef "cloudflare_dns_record.co_ysun_meet.content";
-        comment = tfRef "cloudflare_dns_record.co_ysun_meet.comment";
-      };
-
-      fr_grenug_stats = {
-        type = "CNAME";
-        name = "stats";
-        proxied = tfRef "cloudflare_dns_record.co_ysun_stats.proxied";
-        content = tfRef "cloudflare_dns_record.co_ysun_stats.content";
-        comment = tfRef "cloudflare_dns_record.co_ysun_stats.comment";
       };
 
       fr_grenug_atproto = {
