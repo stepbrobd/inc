815e28b806b843a1ece12393565ac42ed87e7be7
Yifei Sun <ysun@hey.com>
2026-03-27 22:34:01 +0100

    modules/nixos/audio: tweak audio

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -10,9 +10,12 @@
     roomeqwizard
   ];
 
-  # universal audio thunderbolt driver
-  # https://github.com/stepbrobd/uad2
   boot = {
+    # convert hardirq to regular kernel threads scheduler manage
+    # should give audio related higher priority
+    kernelParams = [ "threadirqs" ];
+    # universal audio thunderbolt driver
+    # https://github.com/stepbrobd/uad2
     kernelModules = [ "uad2" ];
     extraModulePackages = [
       (inputs.uad2.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
@@ -40,9 +43,10 @@
       pipewire."92-low-latency" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
-          "default.clock.quantum" = 32;
+          "default.clock.allowed-rates" = [ 44100 48000 88200 96000 176400 192000 ];
+          "default.clock.quantum" = 128;
           "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 32;
+          "default.clock.max-quantum" = 1024;
         };
       };
       pipewire-pulse."92-low-latency" = {
@@ -54,14 +58,14 @@
         ];
         "pulse.properties" = {
           "pulse.min.req" = "32/48000";
-          "pulse.default.req" = "32/48000";
-          "pulse.max.req" = "32/48000";
+          "pulse.default.req" = "128/48000";
+          "pulse.max.req" = "1024/48000";
           "pulse.min.quantum" = "32/48000";
-          "pulse.max.quantum" = "32/48000";
+          "pulse.max.quantum" = "1024/48000";
         };
         "stream.properties" = {
-          "node.latency" = "32/48000";
-          "resample.quality" = 1;
+          "node.latency" = "128/48000";
+          "resample.quality" = 4;
         };
       };
     };
