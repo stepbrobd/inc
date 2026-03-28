e978452e22701844e9b8394fb9722a96f17f8978
Yifei Sun <ysun@hey.com>
2026-03-28 18:39:20 +0100

    lib/mk-colmena: since we forked colmena with darwin deployment cap we can bundle everything in one go

--- lib/mk-colmena.nix
+++ lib/mk-colmena.nix
@@ -1,79 +1,85 @@
 { lib }:
 
 let
-  inherit (lib) genHostModules;
+  inherit (lib) attrNames genAttrs genHostModules map mapAttrs mergeAttrsList;
 in
 { inputs
-, os
-, hosts ? { } # { "x86_64-linux" = [ "host1" ]; "aarch64-linux" = [ "host2" ]; }
-, users ? { } # { "username" -> [ module ] }
-, modules ? [ ] # nixos/darwin modules
+, hosts ? [ ] # [ { os, platform, modules, users, names } ]
 , nixpkgs # raw nixpkgs flake input
+, nix-darwin ? null # nix-darwin flake input
 , getSystem # platform -> perSystem attrset
 , specialArgs ? { }
 }:
 
 let
-  # invert { platform -> [ hosts ] } to { host -> platform }
-  hostPlatforms = lib.mergeAttrsList (lib.mapAttrsToList
-    (platform: hostList: lib.genAttrs hostList (_: platform))
+  # flatten groups into { host -> { os, platform, modules, users } }
+  hostConfigs = mergeAttrsList (map
+    (group: genAttrs group.names (_: {
+      inherit (group) os platform modules users;
+    }))
     hosts);
 
-  allHostNames = lib.attrNames hostPlatforms;
+  allHostNames = attrNames hostConfigs;
 in
 {
   meta = {
     # only used for colmena bootstrapping (lib, eval-config.nix path);
     # each node's actual pkgs comes from nodeNixpkgs
     nixpkgs = import nixpkgs { system = "x86_64-linux"; };
+    # for darwin deployment
+    inherit nix-darwin;
 
-    nodeNixpkgs = lib.mapAttrs
-      (_: platform: (getSystem platform).allModuleArgs.pkgs)
-      hostPlatforms;
+    nodeNixpkgs = mapAttrs
+      (_: cfg: (getSystem cfg.platform).allModuleArgs.pkgs)
+      hostConfigs;
 
-    # nodeSpecialArgs not needed
     inherit specialArgs;
   };
-} // lib.genAttrs allHostNames (host: {
-  imports =
-    let
-      platform = hostPlatforms.${host};
-      entrypoint = "${inputs.self}/hosts/server/${host}";
-    in
-    genHostModules
-      {
-        inherit inputs os platform entrypoint users modules specialArgs;
-      } ++ [
-      (
-        { config, ... }:
+} // genAttrs allHostNames (host:
+  let
+    cfg = hostConfigs.${host};
+    entrypoint = "${inputs.self}/hosts/${host}";
+  in
+  {
+    # at hive node top-level so getNodeSystemType can detect it before evaluation
+    deployment.systemType = cfg.os;
+
+    imports =
+      genHostModules
         {
-          deployment =
-            {
-              targetUser = null;
-              targetHost = "${config.networking.hostName}.${lib.blueprint.tailscale.tailnet}";
+          inherit inputs specialArgs entrypoint;
+          inherit (cfg) os platform modules users;
+        } ++ [
+        (
+          { config, ... }:
+          {
+            deployment =
+              {
+                targetUser = null;
+                targetHost = "${config.networking.hostName}.${lib.blueprint.tailscale.tailnet}";
 
-              # fix hanging issue
-              sshOptions = [
-                "-o"
-                "ConnectTimeout=10"
-                "-o"
-                "ControlMaster=auto"
-                "-o"
-                "ServerAliveCountMax=3"
-                "-o"
-                "ServerAliveInterval=10"
-                "-o"
-                "TCPKeepAlive=no"
-              ];
+                # fix hanging issue
+                sshOptions = [
+                  "-o"
+                  "ConnectTimeout=10"
+                  "-o"
+                  "ControlMaster=auto"
+                  "-o"
+                  "ServerAliveCountMax=3"
+                  "-o"
+                  "ServerAliveInterval=10"
+                  "-o"
+                  "TCPKeepAlive=no"
+                ];
 
-              # inherit all the tags so its easier to filter
-              tags =
-                let
-                  bp = lib.blueprint.hosts.${config.networking.hostName} or null;
-                in
-                if bp != null then bp.tags else [ ];
-            };
-        }
-      )
-    ];
-})
+                # inherit all the tags so its easier to filter
+                tags =
+                  let
+                    bp = lib.blueprint.hosts.${config.networking.hostName} or null;
+                  in
+                  if bp != null then bp.tags else [ ];
+              };
+          }
+        )
+      ];
+  })
