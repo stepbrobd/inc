92db6e00b1670571714b95e6403c16e05303c3f5
Yifei Sun <ysun@hey.com>
2026-03-28 16:34:42 +0100

    pkgs/colmena: bring in darwin support from colmena#319

--- pkgs/colmena/default.nix
+++ pkgs/colmena/default.nix
@@ -1,5 +1,10 @@
 { inputs, stdenv }:
 
 inputs.colmena.packages.${stdenv.hostPlatform.system}.colmena.overrideAttrs (prev: {
-  patches = prev.patches or [ ] ++ [ ./detached-activation.patch ];
+  patches = prev.patches or [ ] ++ [
+    # https://github.com/stepbrobd/colmena/tree/detached
+    ./detached-activation.patch
+    # rebased on top of detached activation https://github.com/zhaofengli/colmena/pull/319
+    ./darwin-colmena.patch
+  ];
 })
