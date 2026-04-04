10fae0caa9149a19b0d4253b11b6e8463a49c28a
Yifei Sun <ysun@hey.com>
2026-04-04 16:42:11 +0200

    hosts/oxide: join mesh

---
+++ hosts/oxide/as10779.nix
@@ -0,0 +1,23 @@
+{ config, lib, ... }:
+
+{
+  services.as10779 = {
+    enable = true;
+    router.exit = false;
+
+    local = {
+      hostname = config.networking.hostName;
+      interface = {
+        local = lib.blueprint.hosts.oxide.ipam.interface;
+        primary = lib.blueprint.hosts.oxide.interface;
+      };
+      ipv4.addresses = [ "${lib.blueprint.hosts.oxide.ipam.ipv4}/32" ];
+      ipv6.addresses = [ "${lib.blueprint.hosts.oxide.ipam.ipv6}/128" ];
+    };
+
+    router.static = {
+      ipv4.routes = [{ option = "reject"; prefix = "23.161.104.0/24"; }];
+      ipv6.routes = [{ option = "reject"; prefix = "2602:f590::/36"; }];
+    };
+  };
+}
