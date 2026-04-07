e9a485c0129bab031d38ce1822ec87204e0d4f2f
Yifei Sun <ysun@hey.com>
2026-04-07 11:14:33 +0200

    pkgs/bird3: update vendored patch commit

--- pkgs/bird3/default.nix
+++ pkgs/bird3/default.nix
@@ -7,12 +7,12 @@ pkgsPrev.bird3.overrideAttrs (oldAttrs: {
   patches = (oldAttrs.patches or [ ]) ++ [
     # link quality algo selection
     (fetchpatch2 {
-      url = "https://github.com/nickcao/bird/commit/2912d03c99f99bbe2f7d5041b43a551d3156ce93.patch";
+      url = "https://github.com/nickcao/bird/commit/427cf93908cce6a6f9faeab9d53972e324b8d357.patch";
       hash = "sha256-osVfPQAw5qcLhirGtSZDSUA8/ZosJNz1utwhO87sL8c=";
     })
     # iface deletion race
     (fetchpatch2 {
-      url = "https://github.com/nickcao/bird/commit/36f9f42912a1adc376886de6a569aff38313326b.patch";
+      url = "https://github.com/nickcao/bird/commit/1a4d4a81b3e3ca23edad75c027dc3e19ef1947b8.patch";
       hash = "sha256-h6WC/EJr+iA+tZbKlDFJfiRmZP8MnSSyY2cJg+JNMXc=";
     })
   ];
