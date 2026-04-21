175d211df6d76c12c970298dc2e515bd117cb5cb
Yifei Sun <ysun@hey.com>
2026-04-21 19:43:39 +0200

    pkgs/nixvim: swap alpha with snacks dashboard

--- pkgs/nixvim/dashboard.nix
+++ pkgs/nixvim/dashboard.nix
@@ -1,100 +1,35 @@
 {
-  plugins.alpha = {
+  plugins.snacks = {
     enable = true;
-    settings.layout = [
-      {
-        type = "padding";
-        val = 10;
-      }
-      {
-        type = "text";
-        val = "not";
-        opts = {
-          position = "center";
-          hl = "Type";
-        };
-      }
-      {
-        type = "padding";
-        val = 1;
-      }
-      {
-        type = "text";
-        val = [
-          "███████╗███╗   ███╗ █████╗  ██████╗███████╗"
-          "██╔════╝████╗ ████║██╔══██╗██╔════╝██╔════╝"
-          "█████╗  ██╔████╔██║███████║██║     ███████╗"
-          "██╔══╝  ██║╚██╔╝██║██╔══██║██║     ╚════██║"
-          "███████╗██║ ╚═╝ ██║██║  ██║╚██████╗███████║"
-          "╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝"
+
+    settings.dashboard = {
+      preset = {
+        header = ''
+          ███████╗███╗   ███╗ █████╗  ██████╗███████╗
+          ██╔════╝████╗ ████║██╔══██╗██╔════╝██╔════╝
+          █████╗  ██╔████╔██║███████║██║     ███████╗
+          ██╔══╝  ██║╚██╔╝██║██╔══██║██║     ╚════██║
+          ███████╗██║ ╚═╝ ██║██║  ██║╚██████╗███████║
+          ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝
+        '';
+        keys = [
+          { icon = " "; key = "n"; desc = "New File"; action = ":ene | startinsert"; }
+          { icon = " "; key = "f"; desc = "Find File"; action.__raw = "function() Snacks.picker.files() end"; }
+          { icon = " "; key = "g"; desc = "Live Grep"; action.__raw = "function() Snacks.picker.grep() end"; }
+          { icon = " "; key = "q"; desc = "Quit"; action = ":qa"; }
         ];
-        opts = {
-          position = "center";
-          hl = "Type";
-        };
-      }
-      {
-        type = "padding";
-        val = 5;
-      }
-      {
-        type = "group";
-        opts.position = "center";
-        val = [
-          {
-            type = "button";
-            val = "      New File ";
-            on_press.__raw = "function() vim.cmd[[ene | startinsert]] end";
-            opts = {
-              position = "center";
-              shortcut = "n";
-              keymap = [ "n" "n" "<cmd>ene | startinsert<cr>" { noremap = true; silent = true; nowait = true; } ];
-            };
-          }
-          {
-            type = "button";
-            val = "      Find File";
-            on_press.__raw = "function() vim.cmd[[Telescope find_files]] end";
-            opts = {
-              position = "center";
-              shortcut = "f";
-              keymap = [ "n" "f" "<cmd>Telescope find_files<cr>" { noremap = true; silent = true; nowait = true; } ];
-            };
-          }
-          {
-            type = "button";
-            val = "      Live Grep";
-            on_press.__raw = "function() vim.cmd[[Telescope live_grep]] end";
-            opts = {
-              position = "center";
-              shortcut = "g";
-              keymap = [ "n" "g" "<cmd>Telescope live_grep<cr>" { noremap = true; silent = true; nowait = true; } ];
-            };
-          }
-          {
-            type = "button";
-            val = "      Quit     ";
-            on_press.__raw = "function() vim.cmd[[qa]] end";
-            opts = {
-              position = "center";
-              shortcut = "q";
-              keymap = [ "n" "q" "<cmd>qa<cr>" { noremap = true; silent = true; nowait = true; } ];
-            };
-          }
-        ];
-      }
-      {
-        type = "padding";
-        val = 5;
-      }
-      {
-        type = "text";
-        val = "its neovim";
-        opts = {
-          position = "center";
-          hl = "Type";
-        };
-      }
-    ];
+      };
+
+      sections = [
+        { padding = 4; }
+        { text = "not"; align = "center"; hl = "Type"; }
+        { padding = 1; }
+        { section = "header"; }
+        { padding = 2; }
+        { section = "keys"; gap = 0; padding = 1; }
+        { padding = 2; }
+        { text = "its neovim"; align = "center"; hl = "Type"; }
+      ];
+    };
   };
 }
