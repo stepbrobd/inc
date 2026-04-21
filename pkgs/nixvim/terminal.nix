d0e3da4cd480d70ada97ce5c95d1c907f4d7bc7d
Yifei Sun <ysun@hey.com>
2026-04-21 19:49:32 +0200

    pkgs/nixvim: toggleterm also swapped with snacks

--- pkgs/nixvim/terminal.nix
+++ pkgs/nixvim/terminal.nix
@@ -1,47 +1,37 @@
 {
-  plugins.toggleterm = {
-    enable = true;
-    settings.shade_terminals = false;
-  };
-
-  plugins.lualine.settings.extensions = [ "toggleterm" ];
-
   keymaps = [
     {
       mode = "n";
       key = "<leader>tf";
-      action = "<cmd>:ToggleTerm direction=float<cr>";
-      options = {
-        silent = true;
-        desc = "Start a new floating terminal";
-      };
+      action.__raw = ''function() Snacks.terminal.toggle(nil, { win = { position = "float" } }) end'';
+      options = { silent = true; desc = "Start a new floating terminal"; };
     }
     {
       mode = "n";
       key = "<leader>th";
-      action = "<cmd>:ToggleTerm direction=horizontal size=20<cr>";
-      options = {
-        silent = true;
-        desc = "Start a new horizontal terminal";
-      };
+      action.__raw = ''function() Snacks.terminal.toggle(nil, { win = { position = "bottom", height = 20 } }) end'';
+      options = { silent = true; desc = "Start a new horizontal terminal"; };
     }
     {
       mode = "n";
       key = "<leader>tv";
-      action = "<cmd>:ToggleTerm direction=vertical size=20<cr>";
-      options = {
-        silent = true;
-        desc = "Start a new vertical terminal";
-      };
+      action.__raw = ''function() Snacks.terminal.toggle(nil, { win = { position = "right",  width  = 60 } }) end'';
+      options = { silent = true; desc = "Start a new vertical terminal"; };
     }
     {
       mode = "n";
       key = "<leader>tx";
-      action = "<cmd>:ToggleTermToggleAll!<cr>";
-      options = {
-        silent = true;
-        desc = "Quit all terminal sessions";
-      };
+      action.__raw = ''
+        function()
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.bo[buf].buftype == "terminal" then
+              vim.api.nvim_win_close(win, true)
+            end
+          end
+        end
+      '';
+      options = { silent = true; desc = "Quit all terminal sessions"; };
     }
   ];
 }
