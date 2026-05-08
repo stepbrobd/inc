f2be1f17ea8e6aaec9277a05953f4437dd3bac29
Yifei Sun <ysun@hey.com>
2026-05-08 12:44:41 +0200

    pkgs/caddy: revert try downgrading cloudflare dns provider and see if the zone walk issue persists
    This reverts commit fe564fb96a12ec3c34cd991f531e5a337e47b46d.

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -3,10 +3,10 @@
 # pull caddy from `inputs` directly to prevent infinite recursion
 # as `caddy.withPlugins` is implemented with override
 inputs.nixpkgs.legacyPackages.${stdenv.hostPlatform.system}.caddy.withPlugins {
-  hash = "sha256-i+a9nB4KPhcI1OHP8YWzE/nRN0h2qkQAPFu7PBrR9Cs=";
+  hash = "sha256-wLWFKd3VO/KP/n68fCpBC+fcCZIRcWHkwTTdxNCU2P0=";
   plugins = [
     "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
-    "github.com/caddy-dns/cloudflare@v0.2.3"
+    "github.com/caddy-dns/cloudflare@v0.2.4"
     "github.com/relvacode/caddy-oidc@v0.3.1"
     "github.com/ss098/certmagic-s3@v0.0.0-20250922022452-8af482af5f39"
     "github.com/ueffel/caddy-brotli@v1.6.0"
