ae9d2fe3455431be693dc7320249c75b96307be8
Yifei Sun <ysun@hey.com>
2026-05-09 18:01:06 +0200

    modules/nixos/acme: use shortlived profile

--- modules/nixos/acme.nix
+++ modules/nixos/acme.nix
@@ -8,7 +8,11 @@
     defaults = {
       email = "ysun@hey.com";
       dnsProvider = "cloudflare";
-      extraLegoFlags = [ "--dns.resolvers=1.1.1.1:53" ];
+      profile = "shortlived";
+      extraLegoFlags = [
+        "--dns.resolvers=[2606:4700:4700::1111]:53"
+        "--dns.resolvers=1.1.1.1:53"
+      ];
       environmentFile = config.sops.secrets.acme.path;
     };
   };
