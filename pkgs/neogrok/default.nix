91980e737acd8148ea60babdd065a3c67cfca2e3
Yifei Sun <ysun@hey.com>
2026-05-12 15:39:46 +0200

    pkgs/neogrok: fix build due to yarn-berry bump

--- pkgs/neogrok/default.nix
+++ pkgs/neogrok/default.nix
@@ -16,11 +16,14 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     hash = "sha256-xAfXT2QioNKHL25lEAlryeaO3JUfOAIMW2+jaobvukY=";
   };
 
+  # https://github.com/NixOS/nixpkgs/pull/513745
+  # https://github.com/NixOS/nixpkgs/issues/513716
+  patches = [ ./yarn-4.14.patch ];
   missingHashes = ./missing-hashes.json;
   offlineCache = yarn-berry.fetchYarnBerryDeps {
     inherit nodejs;
-    inherit (finalAttrs) src missingHashes;
-    hash = "sha256-WtA0wNn3UwDIb85YJhkCubvWrXab+T9tiz6yerdUblM=";
+    inherit (finalAttrs) src missingHashes patches;
+    hash = "sha256-LziXk8kWdeqF4fWjxIR5TDkoYpTj6GQiheGqToCxYnE=";
   };
 
   nativeBuildInputs = [
