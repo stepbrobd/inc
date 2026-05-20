3536a3fc1d0ac6a2ebf4c73dcdbb7a3398c6f740
Yifei Sun <ysun@hey.com>
2026-05-20 11:30:58 +0200

    modules/home/ssh: use the new ssh settings

--- modules/home/ysun/ssh/default.nix
+++ modules/home/ysun/ssh/default.nix
@@ -7,40 +7,39 @@
     enable = true;
 
     enableDefaultConfig = false;
-    matchBlocks."*" = {
-      forwardAgent = lib.mkDefault false;
-      addKeysToAgent = lib.mkDefault "no";
-      compression = lib.mkDefault false;
-      serverAliveInterval = 60;
-      hashKnownHosts = lib.mkDefault false;
-      userKnownHostsFile = lib.concatStringsSep " " [
-        "~/.ssh/known_hosts"
-        "~/.ssh/known_hosts_ca"
-      ];
+    settings = {
+      "g5k" = {
+        WarnWeakCrypto = "no";
+        User = "yisun";
+        Hostname = "access.grid5000.fr";
+      };
+
+      "*.*.g5k" = lib.hm.dag.entryAfter [ "g5k" ] {
+        WarnWeakCrypto = "no";
+        ProxyCommand = ''
+          sh -c 'h=%h; node="''${h%%%%.*}"; site="''${h#*.}"; site="''${site%%.g5k}"; ssh "''${site}.g5k" -W "''${node}:%p"'
+        '';
+      };
+
+      "*.g5k" = lib.hm.dag.entryAfter [ "*.*.g5k" ] {
+        WarnWeakCrypto = "no";
+        User = "yisun";
+        ProxyCommand = ''
+          sh -c 'ssh g5k -W "$(basename %h .g5k):%p"'
+        '';
+      };
+
+      "*" = {
+        ForwardAgent = lib.mkDefault false;
+        AddKeysToAgent = lib.mkDefault "no";
+        Compression = lib.mkDefault false;
+        ServerAliveInterval = 60;
+        HashKnownHosts = lib.mkDefault false;
+        UserKnownHostsFile = [
+          "~/.ssh/known_hosts"
+          "~/.ssh/known_hosts_ca"
+        ];
+      };
     };
-
-    extraConfig = "\n" + ''
-      Host *
-        ForwardAgent no
-        ServerAliveInterval 60
-        Compression no
-        AddKeysToAgent no
-        HashKnownHosts no
-        UserKnownHostsFile ~/.ssh/known_hosts ~/.ssh/known_hosts_ca
-
-      Host g5k
-        WarnWeakCrypto no
-        User yisun
-        Hostname access.grid5000.fr
-
-      Host *.*.g5k
-        WarnWeakCrypto no
-        ProxyCommand sh -c 'h=%h; node="''${h%%%%.*}"; site="''${h#*.}"; site="''${site%%.g5k}"; ssh "''${site}.g5k" -W "''${node}:%p"'
-
-      Host *.g5k
-        WarnWeakCrypto no
-        User yisun
-        ProxyCommand sh -c 'ssh g5k -W "$(basename %h .g5k):%p"'
-    '';
   };
 }
