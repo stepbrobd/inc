8675bd0c274642c3f9e5eee9c4477981de7e0bd6
Yifei Sun <ysun@hey.com>
2026-05-21 11:14:24 +0200

    modules/home/direnv: enable direnv-instant

--- modules/home/ysun/direnv/default.nix
+++ modules/home/ysun/direnv/default.nix
@@ -1,4 +1,10 @@
+{ inputs, ... }:
+
 {
+  imports = [ inputs.direnv-instant.homeModules.direnv-instant ];
+
+  programs.direnv-instant.enable = true;
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
