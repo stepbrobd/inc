b6dd7c0bb3264bf40426373243ac7ba0e17c39e0
Yifei Sun <ysun@hey.com>
2026-05-29 23:33:53 +0200

    pkgs/mac-app-util: wtf

--- pkgs/mac-app-util/default.nix
+++ pkgs/mac-app-util/default.nix
@@ -1,12 +1,17 @@
-{ lib
-, stdenv
-}:
+{ lib, stdenvNoCC }:
 
-# https://github.com/hraban/mac-app-util
-(builtins.getFlake "github:hraban/mac-app-util/8414fa1e2cb775b17793104a9095aabeeada63ef").packages.${stdenv.hostPlatform.system}.default.overrideAttrs {
-  meta = {
-    mainProgram = "mac-app-util";
-    platforms = lib.platforms.darwin;
-    badPlatforms = lib.platforms.linux;
-  };
-}
+if stdenvNoCC.hostPlatform.isDarwin then
+  (lib.getFlake "github:hraban/mac-app-util/8414fa1e2cb775b17793104a9095aabeeada63ef").packages.${stdenvNoCC.hostPlatform.system}.default.overrideAttrs
+  {
+    meta = {
+      mainProgram = "mac-app-util";
+      platforms = lib.platforms.darwin;
+    };
+  }
+else
+  stdenvNoCC.mkDerivation {
+    pname = "mac-app-util";
+    version = "0-unsupported";
+    dontUnpack = true;
+    meta.platforms = lib.platforms.darwin;
+  }
