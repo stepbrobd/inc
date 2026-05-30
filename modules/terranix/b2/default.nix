05de7aa51d55455de9da0632c4e9f571995561f5
Yifei Sun <ysun@hey.com>
2026-05-30 15:59:40 +0200

    modules/terranix/b2: set life cycle rules

--- modules/terranix/b2/default.nix
+++ modules/terranix/b2/default.nix
@@ -15,5 +15,14 @@ in
   resource.b2_bucket.stepbrobd = {
     bucket_name = "stepbrobd";
     bucket_type = "allPublic";
+
+    # b2 keeps every version by default
+    # prune versions 1 day after they are hidden
+    # like repushed nix-cache-info/narinfo or gc-deleted paths, etc.
+    # DO NOT set days_from_uploading_to_hiding as it will hide live cache objects
+    lifecycle_rules = [{
+      file_name_prefix = "";
+      days_from_hiding_to_deleting = 1;
+    }];
   };
 }
