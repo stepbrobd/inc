4203bdf5f1126cbd2406a0b2ab50461c52b23ca5
Yifei Sun <ysun@hey.com>
2026-05-30 13:42:55 +0200

    pkgs/jitsi-meet: simplify

--- pkgs/jitsi-meet/default.nix
+++ pkgs/jitsi-meet/default.nix
@@ -3,6 +3,6 @@
 , # `pkgsPrev` only provided in overlays
 }:
 
-pkgsPrev.jitsi-meet.overrideAttrs (prev: {
+pkgsPrev.jitsi-meet.overrideAttrs {
   patches = pkgs.lib.singleton ./plausible.patch;
-})
+}
