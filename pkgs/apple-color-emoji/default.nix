5461a2cee527c06c0c83413ec56510281a06d774
Yifei Sun <ysun@hey.com>
2026-06-01 14:11:50 +0200

    pkgs/apple-color-emoji: yeah bump bot wont work on this

--- pkgs/apple-color-emoji/default.nix
+++ pkgs/apple-color-emoji/default.nix
@@ -5,11 +5,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "apple-color-emoji";
-  version = "macos-26-20260218-d5729b24";
+  version = "macos-26-20260219-2aa12422";
 
   src = fetchurl {
     url = "https://github.com/samuelngs/apple-emoji-ttf/releases/download/${finalAttrs.version}/AppleColorEmoji-Linux.ttf";
-    hash = "sha256-TvX+SNSkD+cuikrRoJR+GdT+oH1P6Xh+ufZf4YZQRoA=";
+    hash = "sha256-U1oEOvBHBtJEcQWeZHRb/IDWYXraLuo0NdxWINwPUxg=";
   };
 
   dontUnpack = true;
