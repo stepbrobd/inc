67624304ecf72743b91e2681a4935289c5ce854d
Yifei Sun <ysun@hey.com>
2026-06-04 13:15:47 +0200

    pkgs/niks3{,-server}: fix vendor hash

--- pkgs/niks3-server/default.nix
+++ pkgs/niks3-server/default.nix
@@ -1,5 +1,5 @@
 { inputs, stdenv, niks3 }:
 
 inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3-server.overrideAttrs {
-  inherit (niks3) src patches;
+  inherit (niks3) src patches vendorHash;
 }
--- pkgs/niks3/default.nix
+++ pkgs/niks3/default.nix
@@ -3,4 +3,5 @@
 inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3.overrideAttrs (prev: {
   src = inputs.niks3.outPath;
   patches = (prev.patches or [ ]) ++ [ ./429.patch ];
+  vendorHash = "sha256-dxNk5DWBMyahl36RARCu/JfrpQ6RFATKEuDLEhea5RQ=";
 })
