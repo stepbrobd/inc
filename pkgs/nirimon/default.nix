73c199d9babe71ff40cfba38f433647fe3155efb
Yifei Sun <ysun@hey.com>
2026-06-05 14:05:17 +0200

    pkgs/nirimon: 2026.519.0 -> 2026.605.1

--- pkgs/nirimon/default.nix
+++ pkgs/nirimon/default.nix
@@ -1,20 +1,22 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, makeWrapper
+, wl-mirror
 }:
 
 buildGoModule (finalAttrs: {
   pname = "nirimon";
-  version = "2026.519.0";
+  version = "2026.605.1";
 
   src = fetchFromGitHub {
     owner = "stepbrobd";
     repo = "nirimon";
     tag = finalAttrs.version;
-    hash = "sha256-1bKC1QkWorzfg87xwuGgkIOlLWCj0cotYhpZXYSSm9w=";
+    hash = "sha256-mdBl2QuvAYEltGB2kE0EJhQtWSSZ78qdpSzWKreDZUY=";
   };
 
-  vendorHash = "sha256-/Yihk4SM5s1F7KKZsUnG1ZQgHDPKxi/GPL8blPDgUkk=";
+  vendorHash = "sha256-txuaYMyYYalKGQ5RIuPL/ERyDt/eMeo85aZSgx4HbZk=";
 
   ldflags = [
     "-s"
@@ -22,8 +24,14 @@ buildGoModule (finalAttrs: {
     "-X main.Version=${finalAttrs.version}"
   ];
 
+  nativeBuildInputs = [ makeWrapper ];
+
+  postFixup = ''
+    wrapProgram $out/bin/nirimon --prefix PATH : "${lib.makeBinPath [ wl-mirror ]}"
+  '';
+
   meta = {
-    description = "tui monitor configuration tool for niri with visual layout, drag-and-drop, and profile management";
+    description = "TUI monitor configuration tool for niri with visual layout, drag-and-drop, and profile management";
     homepage = "https://github.com/stepbrobd/nirimon";
     license = lib.licenses.asl20;
     mainProgram = "nirimon";
