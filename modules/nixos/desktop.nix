6ebe6b44eb54a3aa70b1777ee5fa84d5f3f12277
Yifei Sun <ysun@hey.com>
2026-06-08 11:00:11 +0200

    modules/nixos/desktop: use wha bg

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -11,7 +11,7 @@ let
   style = pkgs.writeText "gtk.css" ''
     @import url("${pkgs.nordic}/share/themes/Nordic/gtk-3.0/gtk.css");
     window {
-      background-image: url("${inputs.self}/lib/blueprint/users/ysun/wallpapers/wallpaper.jpg");
+      background-image: url("${lib.blueprint.users.ysun.meta.wallpapersDir}/wha.jpg");
       background-size: cover;
       background-position: center;
     }
