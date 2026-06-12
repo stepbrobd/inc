d96583ca6d1ebf17891b9757d1efc9312e1c1415
Yifei Sun <ysun@hey.com>
2026-06-12 16:35:08 +0200

    pkgs/plausible: 3.0.1 -> 3.2.1

--- pkgs/plausible/default.nix
+++ pkgs/plausible/default.nix
@@ -1,6 +1,6 @@
 { pkgsPrev, lib }:
 
-pkgsPrev.plausible.overrideAttrs {
+(pkgsPrev.callPackage ./override.nix { }).overrideAttrs {
   prePatch = ''
     substituteInPlace lib/plausible_web/templates/layout/app.html.heex \
       --replace-fail '</head>' '<script defer data-domain="${lib.blueprint.services.plausible.domain}" src="/js/script.file-downloads.hash.outbound-links.js"></script></head>'
