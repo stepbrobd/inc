340ea9de6c24553b35b8c4a12f4dab338d372ba9
Yifei Sun <ysun@hey.com>
2026-06-13 18:28:21 +0200

    pkgs/swetrix{,-api}: init at 5.2.1

---
+++ pkgs/swetrix-api/default.nix
@@ -0,0 +1,67 @@
+{ lib
+, stdenv
+, buildNpmPackage
+, swetrix
+, nodejs_24
+, makeWrapper
+, autoPatchelfHook
+}:
+
+buildNpmPackage (finalAttrs: {
+  pname = "swetrix-api";
+  inherit (swetrix) version src;
+
+  sourceRoot = "${finalAttrs.src.name}/backend";
+
+  npmDepsHash = "sha256-HamWyCgaNge3GJPAaoPiZJb+WlS44iIUkKbFU3QqWQE=";
+
+  nodejs = nodejs_24;
+
+  nativeBuildInputs = [ makeWrapper autoPatchelfHook ];
+
+  buildInputs = [ stdenv.cc.cc.lib ];
+
+  autoPatchelfIgnoreMissingDeps = [ "libc.musl-x86_64.so.1" ];
+
+  npmFlags = [ "--legacy-peer-deps" ];
+  npmInstallFlags = [ "--force" ];
+
+  npmBuildScript = "deploy:community";
+
+  env = {
+    SENTRYCLI_SKIP_DOWNLOAD = "1";
+    SCARF_ANALYTICS = "false";
+  };
+
+  postBuild = ''
+    npm prune --omit=dev --legacy-peer-deps
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/swetrix-api
+    cp -r dist node_modules migrations package.json $out/share/swetrix-api/
+
+    makeWrapper ${lib.getExe nodejs_24} $out/bin/swetrix-api \
+      --add-flags "$out/share/swetrix-api/dist/main.js" \
+      --chdir "$out/share/swetrix-api" \
+      --set-default NODE_ENV cloud \
+      --set-default IS_PRIMARY_NODE true
+
+    makeWrapper ${lib.getExe nodejs_24} $out/bin/swetrix-api-clickhouse-init \
+      --add-flags "$out/share/swetrix-api/migrations/clickhouse/initialise_selfhosted.js" \
+      --chdir "$out/share/swetrix-api" \
+      --set-default NODE_ENV cloud
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://swetrix.com";
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ stepbrobd ];
+    mainProgram = "swetrix-api";
+    platforms = lib.platforms.linux;
+  };
+})
---
+++ pkgs/swetrix/default.nix
@@ -0,0 +1,66 @@
+{ lib
+, buildNpmPackage
+, fetchFromGitHub
+, nodejs_24
+, makeWrapper
+}:
+
+buildNpmPackage (finalAttrs: {
+  pname = "swetrix";
+  version = "5.2.1";
+
+  src = fetchFromGitHub {
+    owner = "Swetrix";
+    repo = "swetrix";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-hBChUAIXndfqMnablNJC1YXuqoyf+zH8RmTmQDD9xqI=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/web";
+
+  npmDepsHash = "sha256-gp0zG7/j9uaELNz6u5uuQGZBYcZIcZ4R044SDhvGgrA=";
+
+  nodejs = nodejs_24;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  npmFlags = [ "--legacy-peer-deps" ];
+
+  preBuild = ''
+    export NODE_ENV=production
+    export __SELFHOSTED=true
+  '';
+
+  postBuild = ''
+    npm prune --omit=dev --legacy-peer-deps
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/swetrix
+    cp -r build public package.json node_modules $out/share/swetrix/
+
+    makeWrapper ${lib.getExe nodejs_24} $out/bin/swetrix \
+      --add-flags "$out/share/swetrix/node_modules/@react-router/serve/dist/cli.js" \
+      --add-flags "$out/share/swetrix/build/server/index.js" \
+      --chdir "$out/share/swetrix" \
+      --set-default NODE_ENV production \
+      --set __SELFHOSTED true \
+      --set-default PORT 3000
+
+    runHook postInstall
+  '';
+
+  passthru = {
+    inherit (finalAttrs) src version;
+  };
+
+  meta = {
+    homepage = "https://swetrix.com";
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ stepbrobd ];
+    mainProgram = "swetrix";
+    platforms = lib.platforms.linux;
+  };
+})
