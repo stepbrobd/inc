5ca17051f6a5fdad52f0cbbb9ced247528e115e4
Yifei Sun <ysun@hey.com>
2026-06-15 17:46:14 +0200

    modules/nixos/maxmind: apply random delay on maxmind db download

--- modules/nixos/maxmind.nix
+++ modules/nixos/maxmind.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 { config, ... }:
 
 {
@@ -11,6 +13,13 @@
       LicenseKey._secret = config.sops.secrets."maxmind/license".path;
 
       EditionIDs = [ "GeoLite2-ASN" "GeoLite2-City" ];
+    };
+  };
+
+  systemd.timers = lib.mkIf config.services.geoipupdate.enable {
+    geoipupdate.timerConfig = {
+      RandomizedDelaySec = "6d";
+      FixedRandomDelay = true;
     };
   };
 }
