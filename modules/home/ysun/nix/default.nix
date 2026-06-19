45a6a0786dcc603a37ce40be48770c55f26fbf64
Yifei Sun <ysun@hey.com>
2026-06-19 22:40:31 +0200

    secrets: drop unused ones

--- modules/home/ysun/nix/default.nix
+++ modules/home/ysun/nix/default.nix
@@ -1,14 +1,7 @@
 { config, ... }:
 
-let
-  mode = "0400";
-in
 {
-  sops.secrets."nix.conf/access-tokens" = { inherit mode; };
-  sops.secrets."nix.conf/netrc-file" = { inherit mode; };
+  sops.secrets."nix.conf/access-tokens" = { mode = "0400"; };
 
-  nix = {
-    settings.netrc-file = config.sops.secrets."nix.conf/netrc-file".path;
-    extraOptions = "!include ${config.sops.secrets."nix.conf/access-tokens".path}";
-  };
+  nix.extraOptions = "!include ${config.sops.secrets."nix.conf/access-tokens".path}";
 }
