5b69fd206c12e30d2a791624ab32a8edcf8885e9
Yifei Sun <ysun@hey.com>
2026-06-19 19:54:39 +0200

    modules/home/discord: except it fucking crashes

--- modules/home/ysun/discord/default.nix
+++ modules/home/ysun/discord/default.nix
@@ -1,18 +1,12 @@
 { pkgs, ... }:
 
 {
-  programs.discord = {
+  programs.vesktop = {
     enable = pkgs.stdenv.isLinux;
 
-    settings = {
-      BACKGROUND_COLOR = "#2c2d32";
-      IS_MAXIMIZED = false;
-      IS_MINIMIZED = false;
-      SKIP_HOST_UPDATE = true;
-      DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
-    };
+    settings.splashBackground = "#2c2d32";
   };
 
-  xdg.mimeApps.associations.added."x-scheme-handler/discord" = [ "discord.desktop" ];
-  xdg.mimeApps.defaultApplications."x-scheme-handler/discord" = [ "discord.desktop" ];
+  xdg.mimeApps.associations.added."x-scheme-handler/discord" = [ "vesktop.desktop" ];
+  xdg.mimeApps.defaultApplications."x-scheme-handler/discord" = [ "vesktop.desktop" ];
 }
