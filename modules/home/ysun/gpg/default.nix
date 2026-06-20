31c4903f8fb1cc139772f9d9a55312f2e230ad7c
Yifei Sun <ysun@hey.com>
2026-06-20 14:30:33 +0200

    modules/home/gpg: simplify and enable ssh

--- modules/home/ysun/gpg/default.nix
+++ modules/home/ysun/gpg/default.nix
@@ -1,31 +1,22 @@
-{ lib, ... }:
-
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
-  config = lib.mkMerge [
-    {
-      programs.gpg = {
-        enable = true;
-        homedir = "${config.xdg.dataHome}/gnupg";
-        mutableKeys = true;
-        mutableTrust = true;
-        publicKeys = [
-          {
-            source = ./pgp.asc;
-            trust = "ultimate";
-          }
-        ];
-      };
-    }
+  programs.gpg = {
+    enable = true;
+    homedir = "${config.xdg.dataHome}/gnupg";
+    mutableKeys = true;
+    mutableTrust = true;
+    publicKeys = [{
+      source = ./pgp.asc;
+      trust = "ultimate";
+    }];
+  };
 
-    (lib.mkIf pkgs.stdenv.isLinux {
-      services.gpg-agent = {
-        enable = true;
-        extraConfig = ''
-          allow-loopback-pinentry
-        '';
-      };
-    })
-  ];
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    extraConfig = ''
+      allow-loopback-pinentry
+    '';
+  };
 }
