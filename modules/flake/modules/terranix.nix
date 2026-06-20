bf03b9d8223a3d6349ee0403080789c6da8910b0
Yifei Sun <ysun@hey.com>
2026-06-20 20:43:58 +0200

    pkgs/terranix: extract from module

--- modules/flake/modules/terranix.nix
+++ modules/flake/modules/terranix.nix
@@ -1,51 +1,5 @@
-{ config, lib, modulesFor, self, ... }:
+{ modulesFor, ... }:
 
 {
   flake.terranixModules = modulesFor "terranix";
-
-  perSystem = { pkgs, self', system, ... }: {
-    packages.terranixConfiguration =
-      let
-        inherit system;
-        modules = lib.attrValues config.flake.terranixModules;
-      in
-      (lib.terranixConfiguration {
-        inherit system modules;
-      }).overrideAttrs {
-        passthru = {
-          inherit (lib.terranixConfigurationAst {
-            inherit system modules;
-          }) config;
-        };
-      };
-
-    apps.terranix = {
-      type = "app";
-      program = pkgs.writeShellApplication {
-        name = "terranix";
-
-        runtimeInputs = with pkgs; [
-          gnused
-          sops
-          opentofu
-        ];
-
-        text = ''
-          rm -f config.tf.json .terraform.lock.hcl
-          cp ${self'.packages.terranixConfiguration} config.tf.json
-
-          if [[ -v GARNIX_CI ]]; then 
-            export SOPS_AGE_KEY_FILE="$GARNIX_ACTION_PRIVATE_KEY_FILE"
-            pushd ${self.outPath} && popd # depend on flake source root
-          fi
-          eval "$(sops decrypt --extract '["cloudflare"]["backend"]["export"]' ${../../../lib/terranix/secrets.yaml})"
-
-          tofu init
-          tofu apply -auto-approve
-
-          rm -f config.tf.json .terraform.lock.hcl
-        '';
-      };
-    };
-  };
 }
---
+++ pkgs/terranix/default.nix
@@ -0,0 +1,49 @@
+{ inputs
+, lib
+, stdenv
+, writeShellApplication
+, opentofu
+, sops
+}:
+
+let
+  config = lib.terranixConfiguration {
+    inherit (stdenv.hostPlatform) system;
+    modules = lib.attrValues inputs.self.terranixModules;
+  };
+
+  terranixActionFor = action: writeShellApplication {
+    name = "terranix";
+
+    runtimeInputs = [
+      opentofu # already overridden with required plugins
+      sops # for secretes ofc
+    ];
+
+    # assuming SOPS_AGE_KEY_FILE is already set by the executing environment
+    text = ''
+      rm -f config.tf.json .terraform.lock.hcl
+
+      cp ${config} config.tf.json
+
+      eval "$(sops decrypt --extract '["cloudflare"]["backend"]["export"]' ${inputs.self}/lib/terranix/secrets.yaml)"
+
+      tofu init
+
+    '' + action + ''
+
+      rm -f config.tf.json .terraform.lock.hcl
+    '';
+  };
+in
+config.overrideAttrs {
+  passthru = {
+    plan = terranixActionFor ''
+      tofu plan
+    '';
+
+    apply = terranixActionFor ''
+      tofu apply -auto-approve
+    '';
+  };
+}
