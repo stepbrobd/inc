b0ff17788819aadd133c6c175eb26b9a1d513615
Yifei Sun <ysun@hey.com>
2026-06-25 10:25:16 +0200

    pkgs/niks3: fix vendor fod hash

--- pkgs/niks3/default.nix
+++ pkgs/niks3/default.nix
@@ -2,5 +2,5 @@
 
 inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3.overrideAttrs (prev: {
   src = inputs.niks3.outPath;
-  vendorHash = "sha256-zYGAd2N3qGavAlT4MggSME7r04kAVn19N7Nh0L0DK5k=";
+  vendorHash = "sha256-xfpOL8JFfqYz2AncprLAL+9UGglwU1OL1mwMSf6aFBQ=";
 })
