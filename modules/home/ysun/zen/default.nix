db7ee34024a6b61527ae8d199c1b1782b2eb3b45
Yifei Sun <ysun@hey.com>
2026-06-26 22:50:00 +0200

    modules/home/zen: the real default

--- modules/home/ysun/zen/default.nix
+++ modules/home/ysun/zen/default.nix
@@ -5,11 +5,22 @@
 {
   home.packages = [ inputs.zen.packages.${pkgs.stdenv.hostPlatform.system}.default ];
 
-  xdg.mimeApps.defaultApplications = lib.optionalAttrs pkgs.stdenv.isLinux {
-    "text/html" = [ "zen.desktop" ];
-    "x-scheme-handler/http" = [ "zen.desktop" ];
-    "x-scheme-handler/https" = [ "zen.desktop" ];
-    "x-scheme-handler/about" = [ "zen.desktop" ];
-    "x-scheme-handler/unknown" = [ "zen.desktop" ];
-  };
+  xdg.mimeApps.defaultApplications = lib.optionalAttrs pkgs.stdenv.isLinux (
+    lib.genAttrs [
+      "text/html"
+      "application/xhtml+xml"
+      "application/x-extension-htm"
+      "application/x-extension-html"
+      "application/x-extension-shtml"
+      "application/x-extension-xht"
+      "application/x-extension-xhtml"
+      "application/pdf"
+      "application/x-pdf"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "x-scheme-handler/about"
+      "x-scheme-handler/unknown"
+    ]
+      (_: [ "zen.desktop" ])
+  );
 }
