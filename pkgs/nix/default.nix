ac0b7fedec6b08c0e5a32566b0245b03f4af17e4
Yifei Sun <ysun@hey.com>
2026-06-28 15:08:06 +0200

    pkgs/nix: use git

--- pkgs/nix/default.nix
+++ pkgs/nix/default.nix
@@ -1,10 +1,6 @@
 { pkgsPrev, fetchpatch2 }:
 
-pkgsPrev.nixVersions.latest.appendPatches [
-  (fetchpatch2 {
-    url = "https://github.com/stepbrobd/nix/commit/8cddad594dee347882b4e589e759b049495d3597.patch";
-    hash = "sha256-ThYo0hsNWKowVz3guhJHgxrRzDI4euw5BLZ3L3RTVgY=";
-  })
+pkgsPrev.nixVersions.git.appendPatches [
   (fetchpatch2 {
     url = "https://github.com/stepbrobd/nix/commit/49baeb9b817e15305915f02aed876fefb440b26d.patch";
     hash = "sha256-qNFD2BajP+JGAiU3rRbNDJwjezrpzWNaRAoYsgqH/lk=";
