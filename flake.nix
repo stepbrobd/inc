b698a77dfe1e9ac2a3d0e3ffd3f3296d18a97c4f
Yifei Sun <ysun@hey.com>
2026-06-28 12:54:29 +0200

    flake: track nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -49,8 +49,9 @@
 
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/master";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     # a
     autopilot.url = "github:stepbrobd/autopilot";
     autopilot.inputs.nixpkgs.follows = "nixpkgs";
