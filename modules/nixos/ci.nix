2a9abb7b64bd2a409c8bcdb7d789c02f3926f8cd
Yifei Sun <ysun@hey.com>
2026-07-02 13:18:07 +0200

    modules/nixos/ci: setup ci user

---
+++ modules/nixos/ci.nix
@@ -0,0 +1,26 @@
+{ lib, ... }:
+
+{ config, pkgs, ... }:
+
+let
+  hasTag = lib.hasTag config.networking.hostName;
+in
+{
+  config = lib.mkIf (hasTag "ci") {
+    users.groups.ci = { };
+    users.users.ci = {
+      description = "CI";
+      home = "/var/lib/ci";
+      createHome = true;
+      isSystemUser = true;
+
+      shell = pkgs.bash;
+
+      group = "ci";
+      extraGroups = [ "wheel" ];
+
+      # restrict -> no pty/forwarding command execution only for colmena
+      openssh.authorizedKeys.keys = [ "restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGRLXe5W3Xg30YU3C7w1POjVE7CPLYabnxLsvgl2Rtp9" ];
+    };
+  };
+}
