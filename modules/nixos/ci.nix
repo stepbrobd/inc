a523ce40d7b7c9f2ea57b9ae1706897849c438cd
Yifei Sun <ysun@hey.com>
2026-07-02 13:50:11 +0200

    modules/nixos/ci: deny ci key if not in tailscale

--- modules/nixos/ci.nix
+++ modules/nixos/ci.nix
@@ -22,5 +22,8 @@ in
       # restrict -> no pty/forwarding command execution only for colmena
       openssh.authorizedKeys.keys = [ "restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGRLXe5W3Xg30YU3C7w1POjVE7CPLYabnxLsvgl2Rtp9" ];
     };
+
+    # ci key should only works from inside tailnet
+    services.openssh.settings.DenyUsers = [ "ci@!100.64.0.0/10,!fd7a:115c:a1e0::/48,*" ];
   };
 }
