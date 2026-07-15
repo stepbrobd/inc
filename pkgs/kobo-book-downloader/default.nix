be49f8068c5f0375a1549f3aa8a4141686339cbe
Yifei Sun <ysun@hey.com>
2026-07-15 02:32:14 +0200

    pkgs/kobo-book-downloader: init at 0.14.0

---
+++ pkgs/kobo-book-downloader/default.nix
@@ -0,0 +1,61 @@
+{ lib
+, python3Packages
+, fetchFromGitHub
+, nix-update-script
+}:
+
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "kobo-book-downloader";
+  version = "0.14.0";
+  pyproject = true;
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "subdavis";
+    repo = "kobo-book-downloader";
+    tag = finalAttrs.version;
+    hash = "sha256-z1q5kqcyJFbmRzQQyAIjDk3lBholwcKsbrsss5eOumQ=";
+  };
+
+  build-system = [
+    python3Packages.poetry-core
+  ];
+
+  dependencies = with python3Packages; [
+    beautifulsoup4
+    click
+    dataclasses-json
+    flask
+    pycryptodome
+    requests
+    setuptools
+    tabulate
+  ];
+
+  pythonImportsCheck = [
+    "kobodl"
+  ];
+
+  pythonRemoveDeps = [
+    "dataclasses"
+  ];
+
+  pythonRelaxDeps = [
+    "dataclasses-json"
+    "flask"
+    "setuptools"
+    "tabulate"
+  ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "A tool to download and remove DRM from your purchased Kobo.com ebooks and audiobooks";
+    homepage = "https://github.com/subdavis/kobo-book-downloader";
+    changelog = "https://github.com/subdavis/kobo-book-downloader/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.unlicense;
+    maintainers = with lib.maintainers; [ stepbrobd ];
+    mainProgram = "kobodl";
+  };
+})
