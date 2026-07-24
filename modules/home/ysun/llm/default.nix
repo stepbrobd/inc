a75da01985670d2411747522df8d14bf5f13f279
Yifei Sun <ysun@hey.com>
2026-07-24 16:54:20 +0200

    modules/home/llm: try pi

--- modules/home/ysun/llm/default.nix
+++ modules/home/ysun/llm/default.nix
@@ -1,12 +1,5 @@
 {
-  programs.opencode = {
-    enable = false;
-
-    settings.autoupdate = false;
-    tui.theme = "nord";
-  };
-
   programs.claude-code.enable = true;
-
   programs.codex.enable = false;
+  programs.pi-coding-agent.enable = true;
 }
