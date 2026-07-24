d5a10742a387c56c8e60b6380da866193d841a81
Yifei Sun <ysun@hey.com>
2026-07-24 14:53:42 +0200

    modules/home/zen: fix xdg mime list

--- modules/home/ysun/zen/default.nix
+++ modules/home/ysun/zen/default.nix
@@ -5,9 +5,15 @@
 {
   home.packages = [ inputs.zen.packages.${pkgs.stdenv.hostPlatform.system}.twilight ];
 
+  home.sessionVariables = lib.optionalAttrs pkgs.stdenv.isLinux { BROWSER = "zen-twilight"; };
+
+  xdg.mimeApps.enable = pkgs.stdenv.isLinux;
   xdg.mimeApps.defaultApplications = lib.optionalAttrs pkgs.stdenv.isLinux (
     lib.genAttrs [
       "text/html"
+      "text/xml"
+      "application/xml"
+      "application/json"
       "application/xhtml+xml"
       "application/x-extension-htm"
       "application/x-extension-html"
@@ -16,11 +22,12 @@
       "application/x-extension-xhtml"
       "application/pdf"
       "application/x-pdf"
+      "image/svg+xml"
       "x-scheme-handler/http"
       "x-scheme-handler/https"
       "x-scheme-handler/about"
       "x-scheme-handler/unknown"
     ]
-      (_: [ "zen.desktop" ])
+      (_: [ "zen-twilight.desktop" ])
   );
 }
