2e6fef3568c65ca9a087c5ef8c832a05c697ba8d
Yifei Sun <ysun@hey.com>
2026-07-24 15:20:43 +0200

    modules/home/zen: zen's firefox policy with extensions

--- modules/home/ysun/zen/default.nix
+++ modules/home/ysun/zen/default.nix
@@ -2,8 +2,69 @@
 
 { pkgs, ... }:
 
+let
+  inherit (lib.blueprint.services.vaultwarden) domain;
+  bitwarden = "{446900e4-71c2-419f-a6a7-df9c091e268b}";
+  amo = slug: {
+    installation_mode = "normal_installed";
+    install_url = "https://addons.mozilla.org/firefox/downloads/latest/${slug}/latest.xpi";
+  };
+in
 {
-  home.packages = [ inputs.zen.packages.${pkgs.stdenv.hostPlatform.system}.twilight ];
+  home.packages = [
+    (inputs.zen.packages.${pkgs.stdenv.hostPlatform.system}.twilight.override {
+      extraPolicies = {
+        DisableAppUpdate = true;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DontCheckDefaultBrowser = true;
+
+        NoDefaultBookmarks = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+          EmailTracking = true;
+        };
+
+        DNSOverHTTPS.Enabled = false;
+
+        PasswordManagerEnabled = false;
+        OfferToSaveLogins = false;
+        DisableMasterPasswordCreation = true;
+        AutofillCreditCardEnabled = false;
+        AutofillAddressEnabled = false;
+
+        ExtensionSettings = {
+          ${bitwarden} = amo "bitwarden-password-manager" // { default_area = "navbar"; };
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = amo "vimium-ff"; # Vimium
+          "adguardadblocker@adguard.com" = amo "adguard-adblocker";
+          "addon@darkreader.org" = amo "darkreader";
+        };
+        "3rdparty".Extensions.${bitwarden}.environment.base = "https://${domain}";
+
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          FeatureRecommendations = false;
+          UrlbarInterventions = false;
+          SkipOnboarding = true;
+          MoreFromMozilla = false;
+        };
+
+        FirefoxSuggest = {
+          WebSuggestions = false;
+          SponsoredSuggestions = false;
+          ImproveSuggest = false;
+        };
+      };
+    })
+  ];
 
   home.sessionVariables = lib.optionalAttrs pkgs.stdenv.isLinux { BROWSER = "zen-twilight"; };
 
