c1f2d46494a19701f498b2019b49816526f1618d
Yifei Sun <ysun@hey.com>
2026-09-10 12:03:20 +0200

    modules/nixos/as10779: set onlink

--- modules/nixos/as10779.nix
+++ modules/nixos/as10779.nix
@@ -800,11 +800,11 @@ in
             ''}
             ${lib.optionalString (isUpstreamExit && !hasStaticGw4) ''
               read -r _ _ gw4 _ dev4 _ < <(ip -4 route show default table main)
-              [ -n "$gw4" ] && ip -4 route replace default via "$gw4" dev "$dev4" table ${vrfTable} metric 1
+              [ -n "$gw4" ] && ip -4 route replace default via "$gw4" dev "$dev4" table ${vrfTable} metric 1 onlink
             ''}
             ${lib.optionalString (isUpstreamExit && !hasStaticGw6) ''
               read -r _ _ gw6 _ dev6 _ < <(ip -6 route show default table main)
-              [ -n "$gw6" ] && ip -6 route replace default via "$gw6" dev "$dev6" table ${vrfTable} metric 1
+              [ -n "$gw6" ] && ip -6 route replace default via "$gw6" dev "$dev6" table ${vrfTable} metric 1 onlink
             ''}
             ${lib.optionalString isMeshExit ''
               ip -4 route del default table ${vrfTable} metric 1 2>/dev/null || true
