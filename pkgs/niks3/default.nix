805d42adb67b9095cbe711f2aa5884d030c9b6bf
Yifei Sun <ysun@hey.com>
2026-09-12 18:27:50 +0200

    pkgs/niks3{,-server}: drop vendor hash overrides

--- pkgs/niks3/default.nix
+++ pkgs/niks3/default.nix
@@ -1,6 +1,1 @@
-{ inputs, stdenv }:
-
-inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3.overrideAttrs {
-  src = inputs.niks3.outPath;
-  vendorHash = "sha256-dAarYako0R2CQG+t03jSSpWPkP5vCFpyrMssTGKOQn0=";
-}
+{ inputs, stdenv }: inputs.niks3.packages.${stdenv.hostPlatform.system}.niks3
