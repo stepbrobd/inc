01cbe837b1c866411a51440a65baf0544bcba52e
Yifei Sun <ysun@hey.com>
2026-09-14 23:25:24 +0200

    modules/terranix/cloudflare: prepare to move grenug to gh pages

--- modules/terranix/cloudflare/zones/grenug.fr/dns.nix
+++ modules/terranix/cloudflare/zones/grenug.fr/dns.nix
@@ -6,6 +6,14 @@ in
 {
   resource.cloudflare_dns_record = forZone "grenug.fr"
     {
+      fr_grenug_apex = {
+        type = "CNAME";
+        proxied = false;
+        name = "@";
+        content = "gre-nug.github.io";
+        comment = "GitHub Pages";
+      };
+
       fr_grenug_wildcard = {
         type = "CNAME";
         proxied = true;
@@ -20,6 +28,14 @@ in
         name = "_atproto";
         content = ''"did=did:plc:2avqf3fyabzocrmygamzdenj"'';
         comment = "Bluesky - Domain Verification";
+      };
+
+      fr_grenug_gh_verification = {
+        type = "TXT";
+        proxied = false;
+        name = "_github-pages-challenge-gre-nug";
+        content = ''"caa832b25d5ae0e50e2b0819b91ab4"'';
+        comment = "GitHub Pages";
       };
     } // mkPurelyMailRecord
     "grenug.fr"
