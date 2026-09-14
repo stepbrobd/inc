f92962deb629f56288dec3596403998438465a61
Yifei Sun <ysun@hey.com>
2026-09-14 10:19:30 +0200

    pkgs/omp: dont do install check

--- pkgs/omp/default.nix
+++ pkgs/omp/default.nix
@@ -1,6 +1,8 @@
 { inputs, lib, stdenv }:
 
 inputs.llm.packages.${stdenv.hostPlatform.system}.omp.overrideAttrs (
+  { doInstallCheck = false; }
+    //
   lib.optionalAttrs stdenv.hostPlatform.isDarwin {
     __darwinAllowLocalNetworking = true;
   }
