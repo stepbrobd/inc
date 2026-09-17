223969f327eeae2bd16e6dff69d85a7db4b95d27
Yifei Sun <ysun@hey.com>
2026-09-17 19:46:41 +0200

    pkgs/buildGo125Module: drop

--- pkgs/buildGo125Module/default.nix
+++
@@ -1,6 +0,0 @@
-# FIXME:
-# alias EOL buildGo125Module for sops
-# drop after
-# https://github.com/mic92/sops-nix/issues/983
-# https://github.com/mic92/sops-nix/pull/984
-{ pkgsFinal }: pkgsFinal.buildGoModule
