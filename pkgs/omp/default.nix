0dd9f76fac2b47e30396ece4f1cc2f714478cfa6
Yifei Sun <ysun@hey.com>
2026-09-17 12:55:30 +0200

    pkgs/omp: drop install check override

--- pkgs/omp/default.nix
+++ pkgs/omp/default.nix
@@ -1,8 +1,6 @@
 { inputs, lib, stdenv }:
 
 inputs.llm.packages.${stdenv.hostPlatform.system}.omp.overrideAttrs (
-  { doInstallCheck = false; }
-    //
   lib.optionalAttrs stdenv.hostPlatform.isDarwin {
     __darwinAllowLocalNetworking = true;
   }
