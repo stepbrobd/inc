47ba0900a0821ad2e9533cd66e405c0046e186af
Yifei Sun <ysun@hey.com>
2026-09-20 15:29:44 +0200

    pkgs/miroir: init

--- pkgs/miroir/default.nix
+++ pkgs/miroir/default.nix
@@ -1,3 +1,54 @@
-{ inputs, stdenv }:
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, installShellFiles
+, versionCheckHook
+, git
+}:
 
-inputs.miroir.packages.${stdenv.hostPlatform.system}.default
+buildGoModule (finalAttrs: {
+  pname = "miroir";
+  version = "2026.920.0";
+
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "stepbrobd";
+    repo = "miroir";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-BNmmN6l6l+NApxZfWb13BEdYBcDk8X0XACdhuUXktLU=";
+  };
+
+  vendorHash = "sha256-MnLkrU8EjgYfj3Pz4a9KZObn60iR56ASWyhCskbWGiE=";
+
+  subPackages = [ "cmd/miroir" ];
+
+  ldflags = [
+    "-s"
+    "-X main.version=${finalAttrs.version}"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  nativeCheckInputs = [ git ];
+
+  postInstall = ''
+    for shell in bash zsh fish; do
+      installShellCompletion --cmd miroir --$shell <("$out/bin/miroir" completion "$shell")
+    done
+  '';
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru.autobump = true;
+
+  meta = {
+    description = "Repo manager wannabe?";
+    homepage = "https://github.com/stepbrobd/miroir";
+    changelog = "https://github.com/stepbrobd/miroir/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ stepbrobd ];
+    mainProgram = "miroir";
+  };
+})
