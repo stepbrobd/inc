6a93f63b2d9e6cef519c78f7ce3c6fa367905f46
Yifei Sun <ysun@hey.com>
2026-09-20 10:54:34 +0200

    pkgs/miruro: 2026.902.0 -> 2026.920.1

--- pkgs/miruro/default.nix
+++ pkgs/miruro/default.nix
@@ -5,22 +5,24 @@
 
 buildGoModule (finalAttrs: {
   pname = "miruro";
-  version = "2026.902.0";
+  version = "2026.920.1";
 
   __structuredAttrs = true;
-
   __darwinAllowLocalNetworking = true;
 
   src = fetchFromGitHub {
     owner = "stepbrobd";
     repo = "miruro";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-/grNOHIOSeEWnVG1ugLY845jKSAFUXeNZSwVVH7/TIU=";
+    hash = "sha256-Pfy0FKVrfQlHXz1UTY4ltkIHEM57ACFOw4ko7mPQXw0=";
   };
 
-  vendorHash = "sha256-f2+fk+2WNSgw3rydqYX31iy7WvgNECCKvQjLVF03x6Y=";
+  vendorHash = "sha256-wJAloyAqvz4mQ9OzIXfuHsDaXtQRH9STaBqwqKFQh9M=";
 
-  ldflags = [ "-s" "-X main.version=${finalAttrs.version}" ];
+  ldflags = [
+    "-s"
+    "-X main.version=${finalAttrs.version}"
+  ];
 
   passthru.autobump = true;
 
